d47a1d36a772727d059b278daaf21781f252d9f6
Hodor Dell <geoffrey.aulombard@gmail.com>
2022-09-03 23:07:29 +0200

    WIP

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 
+
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
