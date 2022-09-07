d389b250bd7776dab07ad56e4eb119eb5b068fed
Hodor Dell <geoffrey.aulombard@gmail.com>
2022-09-07 22:56:43 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, interval, map, Observable, tap } from 'rxjs';
 
 
 @Component({
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
 
+  interval$!: Observable<string>;
   
   ngOnInit() {
-    
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ? 
+        `je suis ${value} et je suis pair` :
+        `je suis ${value} et je suis impair`),
+        tap(text => this.logger(text))
+    );
   }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
+}
 
 }
